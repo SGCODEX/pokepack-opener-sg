5d79c4c60997ff4c0eabdeeaa2fe02d313ddd233
Shivam Gupta <shivampilot2002@gmail.com>
2025-06-18 22:55:41 +0000

    The buttons are still not visible much lets do one thing, in dark theme

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-border bg-card text-card-foreground hover:bg-accent hover:text-accent-foreground",
+          "border bg-transparent text-foreground border-foreground hover:bg-accent hover:text-accent-foreground dark:text-foreground dark:border-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
